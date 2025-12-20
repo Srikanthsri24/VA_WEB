f0ae502c06db6eb6453ef8f4685ce5f45c314ec6
ippili lalithkumar <ippililalith@gmail.com>
2025-12-20 19:48:21 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/VA_WEB/",
   server: {
     host: "::",
     port: 8080,
