11712a89f6450bd594a6fcf16870c35f519a694d
ippili lalithkumar <ippililalith@gmail.com>
2025-12-24 10:20:05 +0530

    update code 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // ✅ for https://visionariesai.com
+  base: "/",
   server: {
     host: "::",
     port: 8080,
