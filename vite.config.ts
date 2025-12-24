92c55d52d7ad2adb501eb325c8cc772f5330121b
ippili lalithkumar <ippililalith@gmail.com>
2025-12-24 10:39:16 +0530

    change vite.js base point

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // ✅ for https://visionariesai.com
+  base: "/VA_WEB/", // ✅ for https://srikanthsri24.github.io/VA_WEB/
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    ...(mode === "development" ? [componentTagger()] : []),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
